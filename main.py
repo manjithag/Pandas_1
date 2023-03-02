37b1394a047aabf95f213db8351de53e328c1eb7
Manjitha <manjitha.ee@gmail.com>
2023-03-02 11:38:19 +0100

    Commit 1

--- main.py
+++ main.py
@@ -4,5 +4,16 @@ df = pd.read_csv('pokemon_data.csv')
 print(df.head(5))
 
 print("----------------------------------")
-
 print(df.tail(3))
+
+print("----------------------------------")
+print(df.columns)
+
+print("----------------------------------")
+print(df['Name'][0:4])
+
+print("----------------------------------")
+print(df.Name[0:4])
+
+print("----------------------------------")
+print(df[['Name','Type 1','Type 2']][0:4])
